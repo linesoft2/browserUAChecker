d1efbe10c9251b26950b10fcb88619026f3cb4d9
linesoft2 <2306212+linesoft2@user.noreply.gitee.com>
2021-09-28 22:01:39 +0800

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,64 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <b-navbar class="header" :shadow="true" :mobile-burger="false">
+            <template v-slot:brand>
+                <b-navbar-item tag="div" class="title_">
+                    浏览器运行环境展示
+                </b-navbar-item>
+            </template>
+
+        </b-navbar>
+        <home class="container_"></home>
+        <footer class="footer">
+            <div class="content has-text-centered">
+                <p>
+                    本页面所显示的信息基于浏览器的UA（用户代理，User-Agent）得出，该数据可被浏览器/用户等修改，不保证准确性。
+                    <br>
+                    By 双霖（LineSoft）
+                </p>
+            </div>
+        </footer>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import home from './pages/home.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        components: {
+            home
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        /* text-align: center; */
+    }
+
+    .header {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        margin-bottom: 1.25rem;
+    }
+
+    .title_ {
+        width: 100%;
+        text-align: center;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+    }
+
+    .navbar-menu {
+        flex-grow: unset !important;
+    }
+
+    .container_ {
+        margin-left: 1.25rem !important;
+        margin-right: 1.25rem !important;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+//导入Buefy https://buefy.org/
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+Vue.use(Buefy)
+
 Vue.config.productionTip = false
 
 new Vue({
